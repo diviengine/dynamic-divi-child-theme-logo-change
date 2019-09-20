49df24efb2e4278d1634ef59cde57388d3d10522
diviengine <47045017+diviengine@users.noreply.github.com>
2019-09-20 21:33:38 +0100

    Delete two-logos.php

--- two-logos.php
+++
@@ -1,4 +0,0 @@
-<?php ?>
-<img class="main-logo" src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
-<img class="fixed-logo" src="/wp-content/uploads/2019/09/email-logo.png" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="fixed-logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
-<?php ?>

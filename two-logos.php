4a46c9460125f57f66460e7730471e38d415bd95
diviengine <47045017+diviengine@users.noreply.github.com>
2019-09-20 21:30:28 +0100

    Create two-logos.php

---
+++ two-logos.php
@@ -0,0 +1,4 @@
+<?php ?>
+<img class="main-logo" src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
+<img class="fixed-logo" src="http://localhost/bodycommerce/wp-content/uploads/2019/09/email-logo.png" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="fixed-logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
+<?php ?>
